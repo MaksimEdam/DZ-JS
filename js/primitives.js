41fe3e79e687cf91e979b65d6c5eef2dafa4825a
MaksimEdam <Edamenko6@gmail.com>
2021-07-19 21:51:57 +0300

    практика

---
+++ js/primitives.js
@@ -0,0 +1,56 @@
+console.log('primitives');
+
+let stringData = "string data";
+console.log(stringData)
+// typeof - оператор определения типа данных
+
+console.log(typeof stringData);
+
+stringData = "";
+console.log(stringData);
+console.log(typeof stringData);
+
+stringData = "  ";
+console.log(stringData);
+console.log(typeof stringData);
+
+// number
+let number = 1;
+console.log(number);
+console.log(typeof number);
+
+number = 2e53;
+console.log(number);
+console.log(typeof number);
+
+number = Infinity;
+console.log(number);
+console.log(typeof number);
+
+number = NaN; //NaN Not a Number
+console.log(number);
+console.log(typeof number);
+
+//Boolean true || false
+
+let isOnLine = true;
+console.log(isOnLine);
+console.log(typeof isOnLine);
+
+isOnLine = false;
+console.log(isOnLine);
+console.log(typeof isOnLine);
+
+// undefined 
+let message;
+console.log(message);
+console.log(typeof message);
+
+// null
+let user = null;
+console.log(user);
+console.log(typeof user);
+
+
+
+
---
+++ js/variables.js
@@ -0,0 +1,25 @@
+/* console.log(student); */
+var student = "Maksim";
+console.log(student);
+
+var count;
+console.log(count);
+
+count = 1;
+console.log(count);
+count = 2;
+console.log(count);
+
+let number = 30;
+console.log(number);
+
+let age;
+console.log(age);
+age = 18;
+console.log(age);
+age = 25;
+console.log(age);
+
+const userName = "Inna";
+console.log(userName); 
+//userName =
